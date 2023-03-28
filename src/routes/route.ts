a1748616757f3a66d717085fb6a993dbb0712b9c
wanderson-rpf <wandersonrpf@gmail.com>
2023-03-28 11:01:43 -0300

    :wrench: [chore] setting route

--- src/routes/route.ts
+++ src/routes/route.ts
@@ -2,8 +2,6 @@ import { Router } from "express";
 
 const router = Router();
 
-router.post('/users', (req, res) => {
-  return res.status(201).send();
-});
+router.post('/users', (req, res) => res.status(201).send());
 
 export { router };
